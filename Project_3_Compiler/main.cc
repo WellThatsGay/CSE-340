ab531572f9a043480b90fb403b8d846b8f653102
WellThatsGay <68203618+WellThatsGay@users.noreply.github.com>
2024-04-18 18:47:06 +0000

    Created a basic parser and a basic compiler

---
+++ Project_3_Compiler/main.cc
@@ -0,0 +1,527 @@
+#include "execute.h"
+#include "lexer.h"
+#include <vector>
+#include <algorithm>
+
+/* Christopher Harris*/
+
+LexicalAnalyzer lexer;
+using namespace std;
+
+vector<string> table;
+
+struct InstructionNode * parse_body();
+
+
+int location(string str) {
+	for (int i = 0; i < table.size(); i++) {
+		if (table[i] == str)
+			return i;
+	}
+}
+
+void parse_id_list() {
+	Token t = lexer.GetToken();
+
+	while (t.token_type != SEMICOLON) {
+
+		table.push_back(t.lexeme);
+		mem[location(t.lexeme)] = 0;
+		t = lexer.GetToken();
+
+	}
+
+}
+
+struct InstructionNode * parse_assign_stmt() {
+	struct InstructionNode * instruction = new InstructionNode;
+	instruction->next = NULL;
+
+	Token t = lexer.GetToken();
+	if (t.token_type == ID) {
+		instruction->type = ASSIGN;
+		instruction->assign_inst.left_hand_side_index = location(t.lexeme);
+	}
+
+	t = lexer.GetToken();
+	if (t.token_type == EQUAL) {
+		t = lexer.GetToken();
+
+		switch (t.token_type)
+		{
+		case ID:
+			instruction->assign_inst.operand1_index = location(t.lexeme);
+			break;
+		case NUM:
+			if (find(table.begin(), table.end(), t.lexeme) == table.end()) {
+				table.push_back(t.lexeme);
+				mem[location(t.lexeme)] = stoi(t.lexeme);
+			}
+
+			instruction->assign_inst.operand1_index = location(t.lexeme);
+			break;
+		}
+
+		t = lexer.GetToken();
+		if (t.token_type == SEMICOLON) {
+			instruction->assign_inst.op = OPERATOR_NONE;
+			instruction->assign_inst.operand2_index = NULL;
+		}
+		else {
+			if (t.token_type == PLUS)
+				instruction->assign_inst.op = OPERATOR_PLUS;
+			else if (t.token_type == MINUS)
+				instruction->assign_inst.op = OPERATOR_MINUS;
+			else if (t.token_type == MULT)
+				instruction->assign_inst.op = OPERATOR_MULT;
+			else if (t.token_type == DIV)
+				instruction->assign_inst.op = OPERATOR_DIV;
+
+			t = lexer.GetToken();
+			switch (t.token_type)
+			{
+			case ID:
+				instruction->assign_inst.operand2_index = location(t.lexeme);
+				break;
+			case NUM:
+				if (find(table.begin(), table.end(), t.lexeme) == table.end()) {
+					table.push_back(t.lexeme);
+					mem[location(t.lexeme)] = stoi(t.lexeme);
+				}
+
+				instruction->assign_inst.operand2_index = location(t.lexeme);
+				break;
+			}
+			t = lexer.GetToken();// get smeicolon
+		}
+	}
+	return instruction;
+}
+
+struct InstructionNode * parse_while_stmt() {
+	struct InstructionNode * instruction = new InstructionNode;
+	instruction->next = NULL;
+
+	Token t = lexer.GetToken();
+	if (t.token_type == WHILE) {
+
+		instruction->type = CJMP;
+		//operand 1
+		t = lexer.GetToken();
+		switch (t.token_type)
+		{
+		case ID:
+			instruction->cjmp_inst.operand1_index = location(t.lexeme);
+			break;
+		case NUM:
+			if (find(table.begin(), table.end(), t.lexeme) == table.end()) {
+				table.push_back(t.lexeme);
+				mem[location(t.lexeme)] = stoi(t.lexeme);
+			}
+			instruction->cjmp_inst.operand1_index = location(t.lexeme);
+			break;
+		}
+		//condition op
+		t = lexer.GetToken();
+		if (t.token_type == GREATER)
+			instruction->cjmp_inst.condition_op = CONDITION_GREATER;
+		else if (t.token_type == LESS)
+			instruction->cjmp_inst.condition_op = CONDITION_LESS;
+		else if (t.token_type == NOTEQUAL)
+			instruction->cjmp_inst.condition_op = CONDITION_NOTEQUAL;
+
+		//operand 2
+		t = lexer.GetToken();
+		switch (t.token_type)
+		{
+		case ID:
+			instruction->cjmp_inst.operand2_index = location(t.lexeme);
+			break;
+		case NUM:
+			if (find(table.begin(), table.end(), t.lexeme) == table.end()) {
+				table.push_back(t.lexeme);
+				mem[location(t.lexeme)] = stoi(t.lexeme);
+			}
+
+			instruction->cjmp_inst.operand2_index = location(t.lexeme);
+			break;
+		}
+
+		instruction->next = parse_body();
+
+		struct InstructionNode* jumpNode = new InstructionNode;
+		jumpNode->type = JMP;
+		jumpNode->jmp_inst.target = instruction;
+
+		struct InstructionNode* temp = instruction->next;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = jumpNode;
+
+		struct InstructionNode * noop = new InstructionNode;
+		noop->type = NOOP;
+		noop->next = NULL;
+		jumpNode->next = noop;
+
+		instruction->cjmp_inst.target = noop;
+	}
+	return instruction;
+}
+
+struct InstructionNode * parse_if_stmt() {
+	struct InstructionNode * instruction = new InstructionNode;
+	instruction->next = NULL;
+
+	Token t = lexer.GetToken();
+	if (t.token_type == IF)
+	{
+		instruction->type = CJMP;
+		//operand 1
+		t = lexer.GetToken();
+		switch (t.token_type)
+		{
+		case ID:
+			instruction->cjmp_inst.operand1_index = location(t.lexeme);
+			break;
+		case NUM:
+			if (find(table.begin(), table.end(), t.lexeme) == table.end()) {
+				table.push_back(t.lexeme);
+				mem[location(t.lexeme)] = stoi(t.lexeme);
+			}
+			instruction->cjmp_inst.operand1_index = location(t.lexeme);
+			break;
+		}
+		//condition op
+		t = lexer.GetToken();
+		if (t.token_type == GREATER)
+			instruction->cjmp_inst.condition_op = CONDITION_GREATER;
+		else if (t.token_type == LESS)
+			instruction->cjmp_inst.condition_op = CONDITION_LESS;
+		else if (t.token_type == NOTEQUAL)
+			instruction->cjmp_inst.condition_op = CONDITION_NOTEQUAL;
+
+		//operand 2
+		t = lexer.GetToken();
+		switch (t.token_type)
+		{
+		case ID:
+			instruction->cjmp_inst.operand2_index = location(t.lexeme);
+			break;
+		case NUM:
+			if (find(table.begin(), table.end(), t.lexeme) == table.end()) {
+				table.push_back(t.lexeme);
+				mem[location(t.lexeme)] = stoi(t.lexeme);
+			}
+
+			instruction->cjmp_inst.operand2_index = location(t.lexeme);
+			break;
+		}
+
+		instruction->next = parse_body();
+
+		struct InstructionNode * noop = new InstructionNode;
+		noop->type = NOOP;
+		noop->next = NULL;
+		
+		struct InstructionNode * temp = instruction->next;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = noop;
+		instruction->cjmp_inst.target = noop;
+	}
+	return instruction;
+}
+
+struct InstructionNode * parse_case(InstructionNode * jump, int temp_operand) {
+	struct InstructionNode * instruction = new InstructionNode;
+
+	instruction->type = CJMP;
+	instruction->cjmp_inst.operand1_index = temp_operand;
+	instruction->cjmp_inst.condition_op = CONDITION_NOTEQUAL;
+
+	Token t = lexer.GetToken(); // case or defualt
+	switch (t.token_type) {
+		case CASE:
+			t = lexer.GetToken();
+			if (t.token_type == ID) {
+				instruction->cjmp_inst.operand2_index = location(t.lexeme);
+			}
+			else if (t.token_type == NUM) {
+				if (find(table.begin(), table.end(), t.lexeme) == table.end()) {
+					table.push_back(t.lexeme);
+					mem[location(t.lexeme)] = stoi(t.lexeme);
+				}
+				instruction->cjmp_inst.operand2_index = location(t.lexeme);
+				break;
+			}		
+		case DEFAULT:
+			instruction->cjmp_inst.operand2_index = temp_operand;
+			break;
+	}
+
+	t = lexer.GetToken(); // colon
+
+	instruction->cjmp_inst.target = parse_body();
+
+	struct InstructionNode * temp = instruction->cjmp_inst.target;
+	while (temp->next != NULL) {
+		temp = temp->next;
+	}
+	temp->next = jump;
+
+	t = lexer.GetToken();
+	lexer.UngetToken(t);
+	if (t.token_type == DEFAULT || t.token_type == CASE) {
+		
+		struct InstructionNode * i = parse_case(jump, temp_operand);
+		instruction->next = i;
+	}
+
+	return instruction;
+}
+
+struct InstructionNode * parse_switch_stmt() {
+	struct InstructionNode * insturction;
+	struct InstructionNode * jump = new InstructionNode;
+	struct InstructionNode * noop = new InstructionNode;
+
+	noop->type = NOOP;
+	noop->next = NULL;
+	jump->type = JMP;
+	jump->jmp_inst.target = noop;
+
+	int temp_opreand;
+
+	Token t = lexer.GetToken();
+	if (t.token_type == SWITCH) {
+		
+		t = lexer.GetToken();
+		if(t.token_type == ID) 
+			temp_opreand = location(t.lexeme);
+		else if (t.token_type == NUM) {
+			if (find(table.begin(), table.end(), t.lexeme) == table.end()) {
+				table.push_back(t.lexeme);
+				mem[location(t.lexeme)] = stoi(t.lexeme);
+			}
+
+			temp_opreand = location(t.lexeme);
+		}
+
+		t = lexer.GetToken();// {
+
+		insturction = parse_case(jump, temp_opreand);
+
+		struct InstructionNode * temp = insturction;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = noop;
+	
+		t = lexer.GetToken(); //}
+
+		return insturction;
+	}
+}
+
+struct InstructionNode * parse_for_stmt() {
+	struct InstructionNode * instruction = new InstructionNode;
+	struct InstructionNode * noop = new InstructionNode;
+	struct InstructionNode * jump = new InstructionNode;
+	
+	instruction->type = CJMP;
+	noop->type = NOOP;
+	jump->type = JMP;
+	instruction->cjmp_inst.target = noop;
+	jump->jmp_inst.target = instruction;
+	jump->next = noop;
+	noop->next = NULL;
+
+	Token t = lexer.GetToken(); // for
+	t = lexer.GetToken();  // (
+	struct InstructionNode * i1 = parse_assign_stmt(); // asign 1
+	i1->next = instruction; 
+
+	t = lexer.GetToken(); // id or num
+	if (t.token_type == ID)
+		instruction->cjmp_inst.operand1_index = location(t.lexeme);
+	else if (t.token_type == NUM) {
+		if (find(table.begin(), table.end(), t.lexeme) == table.end()) {
+			table.push_back(t.lexeme);
+			mem[location(t.lexeme)] = stoi(t.lexeme);
+		}
+		instruction->cjmp_inst.operand1_index = location(t.lexeme);
+	}
+
+	t = lexer.GetToken(); // condtion
+	switch (t.token_type) {
+		case GREATER:
+			instruction->cjmp_inst.condition_op = CONDITION_GREATER;
+		case LESS:
+			instruction->cjmp_inst.condition_op = CONDITION_LESS;
+		case NOTEQUAL:
+			instruction->cjmp_inst.condition_op = CONDITION_NOTEQUAL;
+	}
+
+	t = lexer.GetToken(); // id or num
+	if (t.token_type == ID)
+		instruction->cjmp_inst.operand2_index = location(t.lexeme);
+	else if (t.token_type == NUM) {
+		if (find(table.begin(), table.end(), t.lexeme) == table.end()) {
+			table.push_back(t.lexeme);
+			mem[location(t.lexeme)] = stoi(t.lexeme);
+		}
+		instruction->cjmp_inst.operand2_index = location(t.lexeme);
+	}
+	
+	t = lexer.GetToken(); //  ;
+	struct InstructionNode * i2 = parse_assign_stmt(); // assign 2
+	t = lexer.GetToken();  // )
+	struct InstructionNode * i_list = parse_body();
+	instruction->next = i_list;
+
+	struct InstructionNode * temp = i_list;
+	while (temp->next != NULL) {
+		temp = temp->next;
+	}
+	temp->next = i2;
+	
+	i2->next = jump;
+	return i1;
+}
+
+struct InstructionNode * parse_output_stmt() {
+	struct InstructionNode * instruction = new InstructionNode;
+	instruction->next = NULL;
+
+	Token t = lexer.GetToken(); // OUTPUT
+	instruction->type = OUT;
+
+	t = lexer.GetToken(); // ID
+	instruction->input_inst.var_index = location(t.lexeme);
+
+	t = lexer.GetToken(); // SEMICOLON
+
+	return instruction;
+} 
+
+struct InstructionNode * parse_input_stmt() {
+	struct InstructionNode * instruction = new InstructionNode;
+	instruction->next = NULL;
+
+	Token t = lexer.GetToken(); // INPUT
+	instruction->type = IN;
+
+	t = lexer.GetToken(); // ID
+	instruction->input_inst.var_index = location(t.lexeme);
+
+	t = lexer.GetToken(); // SEMICOLON
+
+	return instruction;
+} 
+
+struct InstructionNode * parse_stmt() {
+	struct InstructionNode * i1;
+	Token t = lexer.GetToken();
+	switch (t.token_type) 
+	{
+		case ID:
+			lexer.UngetToken(t);
+			i1 = parse_assign_stmt();
+			break;
+		case WHILE:
+			lexer.UngetToken(t);
+			i1 = parse_while_stmt();
+			break;
+		case IF:
+			lexer.UngetToken(t);
+			i1 = parse_if_stmt();
+			break;
+		case SWITCH:
+			lexer.UngetToken(t);
+			i1 = parse_switch_stmt();
+			break;
+		case FOR:
+			lexer.UngetToken(t);
+			i1 = parse_for_stmt();
+			break;
+		case OUTPUT:
+			lexer.UngetToken(t);
+			i1 = parse_output_stmt();
+			break;
+		case INPUT:
+			lexer.UngetToken(t);
+			i1 = parse_input_stmt();
+			break;
+	}
+	return i1;
+}
+
+struct InstructionNode * parse_stmt_list() {
+	struct InstructionNode * i1;
+	struct InstructionNode * i2;
+	Token t = lexer.GetToken();
+	TokenType t_type = t.token_type;
+	lexer.UngetToken(t);
+
+	i1 = parse_stmt();
+
+	t = lexer.GetToken();
+	if (t.token_type == ID || t.token_type == WHILE || t.token_type == IF ||
+		t.token_type == SWITCH || t.token_type == FOR || t.token_type == OUTPUT || t.token_type == INPUT) {
+
+		lexer.UngetToken(t);
+		i2 = parse_stmt_list();
+
+		if (i1->type == CJMP || t_type == FOR) {
+			struct InstructionNode * temp = i1->next;
+			while (temp->next != NULL)
+			{
+				temp = temp->next;
+			}
+			temp->next = i2;
+		}
+		else {
+			i1->next = i2;
+		}
+	}
+	else {
+		lexer.UngetToken(t);
+	}
+	return i1;
+}
+
+struct InstructionNode * parse_body() {
+	struct InstructionNode * instruction;
+	Token t = lexer.GetToken();
+	//get left brace
+	if (t.token_type == LBRACE)
+	{
+		instruction = parse_stmt_list();
+	}
+	//get right brace
+	t = lexer.GetToken();
+	
+	return instruction;
+}
+
+void parse_inputs() {
+	Token t = lexer.GetToken();
+	inputs.push_back(stoi(t.lexeme));
+	t = lexer.GetToken(); lexer.UngetToken(t);
+	if (t.token_type == NUM)
+		parse_inputs();
+}
+
+struct InstructionNode * parse_generate_intermediate_representation() {
+	// first line is the delcartion of variables
+	parse_id_list(); 
+	//parse body
+	struct InstructionNode * temp = parse_body();
+	//parse last line which is input
+	parse_inputs();
+	return temp;
+}
+
---
+++ Project_3_Compiler/parser.cc
@@ -0,0 +1,298 @@
+#include <iostream>
+#include "parser.h"
+
+/* Christopher Harris*/
+
+using namespace std;
+
+void Parser::syntax_error() {
+	cout << "SYNTAX ERROR !!!\n";
+	exit(1);
+}
+
+Token Parser::expect(TokenType expected_type){
+	Token t = lexer.GetToken();
+	if (t.token_type != expected_type)
+		syntax_error();
+	return t;
+}
+
+Token Parser::peek(){
+	Token t = lexer.GetToken();
+	lexer.UngetToken(t);
+	return t;
+}
+
+void Parser::parse_program() {
+	parse_var_sction();
+	parse_body();
+	parse_inputs();
+
+	if (peek().token_type != END_OF_FILE)
+		syntax_error();
+}
+
+void Parser::parse_var_sction() {
+	parse_id_list();
+
+	Token t = lexer.GetToken();
+	if (t.token_type != SEMICOLON)
+		syntax_error();
+}
+
+void Parser::parse_id_list() {
+	Token t = lexer.GetToken();
+	if (t.token_type != ID)
+		syntax_error();
+
+	if (peek().token_type == COMMA) {
+		t = lexer.GetToken();
+		parse_id_list();
+	}
+}
+
+void Parser::parse_body() {
+	Token t = lexer.GetToken();
+	if (t.token_type != LBRACE)
+		syntax_error();
+
+	parse_stmt_list();
+
+	t = lexer.GetToken();
+	if (t.token_type != RBRACE)
+		syntax_error();
+}
+
+void Parser::parse_stmt_list() {
+
+	parse_stmt();
+
+	Token t = peek();
+	if (t.token_type == ID || t.token_type == WHILE || t.token_type == IF || t.token_type == SWITCH || t.token_type == FOR || t.token_type == OUTPUT || t.token_type == INPUT)
+		parse_stmt_list();
+}
+
+void Parser::parse_stmt() {
+	if (peek().token_type == ID)
+		parse_assign_stmt();
+	else if (peek().token_type == WHILE)
+		parse_while_stmt();
+	else if (peek().token_type == IF)
+		parse_if_stmt();
+	else if (peek().token_type == SWITCH)
+		parse_switch_stmt();
+	else if (peek().token_type == FOR)
+		parse_for_stmt();
+	else if (peek().token_type == OUTPUT)
+		parse_output_stmt();
+	else if (peek().token_type == INPUT)
+		parse_input_stmt();
+	else
+		syntax_error();
+}
+
+void Parser::parse_assign_stmt() {
+	Token t = lexer.GetToken();
+	if (t.token_type != ID)
+		syntax_error();
+
+	t = lexer.GetToken();
+	if (t.token_type != EQUAL)
+		syntax_error();
+
+	t = lexer.GetToken();
+	if (peek().token_type == PLUS || peek().token_type == MINUS || peek().token_type == MULT || peek().token_type == DIV)
+	{
+		lexer.UngetToken(t);
+		parse_expr();
+	}else{
+		lexer.UngetToken(t);
+		parse_primary();
+	}
+
+	t = lexer.GetToken();
+	if (t.token_type != SEMICOLON)
+		syntax_error();
+}
+
+void Parser::parse_expr() {
+	parse_primary();
+	parse_op();
+	parse_primary();
+}
+
+void Parser::parse_primary() {
+	Token t = lexer.GetToken();
+	if (t.token_type == ID || t.token_type == NUM)
+		return;
+	else
+		syntax_error();
+}
+
+void Parser::parse_op() {
+	Token t = lexer.GetToken();
+	if (t.token_type == PLUS || t.token_type == MINUS || t.token_type == MULT || t.token_type == DIV)
+		return;
+	else
+		syntax_error();
+}
+
+void Parser::parse_output_stmt() {
+	Token t = lexer.GetToken();
+	if (t.token_type != OUTPUT)
+		syntax_error();
+
+	t = lexer.GetToken();
+	if (t.token_type != ID)
+		syntax_error();
+
+	t = lexer.GetToken();
+	if (t.token_type != SEMICOLON)
+		syntax_error();
+}
+
+void Parser::parse_input_stmt() {
+	Token t = lexer.GetToken();
+	if (t.token_type != INPUT)
+		syntax_error();
+
+	t = lexer.GetToken();
+	if (t.token_type != ID)
+		syntax_error();
+
+	t = lexer.GetToken();
+	if (t.token_type != SEMICOLON)
+		syntax_error();
+}
+
+void Parser::parse_while_stmt() {
+	Token t = lexer.GetToken();
+	if (t.token_type != WHILE)
+		syntax_error();
+
+	parse_condition();
+	parse_body();
+}
+
+void Parser::parse_if_stmt() {
+	Token t = lexer.GetToken();
+	if (t.token_type != IF)
+		syntax_error();
+
+	parse_condition();
+	parse_body();
+}
+
+void Parser::parse_condition() {
+	parse_primary();
+	parse_relop();
+	parse_primary();
+}
+
+void Parser::parse_relop() {
+	Token t = lexer.GetToken();
+	if (t.token_type == GREATER)
+		return;
+	else if (t.token_type == LESS)
+		return;
+	else if (t.token_type == NOTEQUAL)
+		return;
+	else
+		syntax_error();
+}
+
+void Parser::parse_switch_stmt() {
+	Token t = lexer.GetToken();
+	if (t.token_type != SWITCH)
+		syntax_error();
+
+	t = lexer.GetToken();
+	if (t.token_type != ID)
+		syntax_error();
+
+	t = lexer.GetToken();
+	if (t.token_type != LBRACE)
+		syntax_error();
+
+	parse_case();
+
+	if (peek().token_type == DEFAULT)
+		parse_default_case();
+
+	t = lexer.GetToken();
+	if (t.token_type != RBRACE)
+		syntax_error();
+}
+
+void Parser::parse_for_stmt() {
+	Token t = lexer.GetToken();
+	if (t.token_type != FOR)
+		syntax_error();
+
+	t = lexer.GetToken();
+	if (t.token_type != LPAREN)
+		syntax_error();
+
+	parse_assign_stmt();
+	parse_condition();
+
+	t = lexer.GetToken();
+	if (t.token_type = SEMICOLON)
+		syntax_error();
+
+	parse_assign_stmt();
+
+	t = lexer.GetToken();
+	if (t.token_type != RPAREN)
+		syntax_error();
+
+	parse_body();
+}
+
+void Parser::parse_case_list() {
+	parse_case();
+
+	if (peek().token_type == CASE)
+		parse_case_list();
+}
+
+void Parser::parse_case() {
+	Token t = lexer.GetToken();
+	if (t.token_type != CASE)
+		syntax_error();
+
+	t = lexer.GetToken();
+	if (t.token_type != NUM)
+		syntax_error();
+
+	t = lexer.GetToken();
+	if (t.token_type != COLON)
+		syntax_error();
+
+	parse_body();
+}
+
+void Parser::parse_default_case() {
+	Token t = lexer.GetToken();
+	if (t.token_type != DEFAULT)
+		syntax_error();
+
+	t = lexer.GetToken();
+	if (t.token_type != COLON)
+		syntax_error();
+
+	parse_body();
+}
+
+void Parser::parse_inputs() {
+	parse_num_list();
+}
+
+void Parser::parse_num_list() {
+	Token t = lexer.GetToken();
+	if (t.token_type != NUM)
+		syntax_error();
+
+	if (peek().token_type == NUM)
+		parse_num_list();
+}
